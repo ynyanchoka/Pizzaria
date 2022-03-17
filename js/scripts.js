1a04b025b95e486f99d82f7daccd6790564233e4
ynyanchoka <monaryymelda@gmail.com>
2022-03-17 21:06:37 +0300

    add footer

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,3 @@
   
 
 
-// function showslide(){
-//     var i;
-//     var slides = document.getElementsByClassName("slide-item");
-//     var dots = document.getElementsByClassName("dot");
-//     for (var i = 0; i < slideIndex.length; i++){
-//         slides[i].style.display = "none";
-//     }
-//     for (let i = 0; i < dots.length; i++){
-//         dots[i].className = dots[i].className.replace("active","");
-//     }
-//     slideIndex++;
-//     if (slideIndex>slides.length) slideIndex = 1;
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].className+="active"; 
-    
-// }
